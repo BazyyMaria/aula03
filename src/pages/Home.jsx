f5cccd06f4072f72abf66f50cf8b2f2e2669aecb
Maria Eduarda <mariaeduarda34418780@gmail.com>
2024-11-11 20:21:06 -0300

    ptac2 primeiro commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react";
+
+export default function Home() {
+    //falta o estado aqui
+}
+
+useEffect(() => {
+    const receberListaProdutos = async () => {
+        try {
+            //falta fetch aqui
+            //falta formatar de json para array
+
+        } catch (erro) {
+            //falta um console.error()
+         }
+      }
+    receberListaProdutos();
+},[]); 
